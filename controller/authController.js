0913869762771c94f14cf8ac74509e0f005eeda4
Razim Rafi <8910razim@gmail.com>
2024-12-18 23:34:53 +0530

    auth config change

--- controller/authController.js
+++ controller/authController.js
@@ -39,7 +39,7 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 60,
-            sameSite: "lax",
+            sameSite: "None",
             secure: process.env.PRODUCTION == 1 ,
         });
 
